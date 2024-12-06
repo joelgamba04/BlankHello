0056bc1d4ceae5c1cf8be8522a559bad86efde1c
Jose Noel C. Gamba <joelgamba04@gmail.com>
2024-12-06 12:32:32 +0800

    Initial Commit

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Slot } from "expo-router";
+import "../global.css";
+
+// const RootLayout = () => {
+//   return (
+//     <>
+//       <Stack>
+//         <Stack.Screen name="index" options={{ headerShown: false }} />
+//       </Stack>
+//     </>
+//   );
+// };
+
+export default Slot;
